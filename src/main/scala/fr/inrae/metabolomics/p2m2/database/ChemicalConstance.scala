d392ea9021c76e6c85a087fd2158ee10300be575
Olivier Filangi <you@example.com>
2022-10-07 10:23:53 +0200

    add brachemdb information csv final results

---
+++ src/main/scala/fr/inrae/metabolomics/p2m2/database/ChemicalConstance.scala
@@ -0,0 +1,10 @@
+package fr.inrae.metabolomics.p2m2.database
+
+case object ChemicalConstance {
+  val massProton: Double = 1.007276
+  val massNeutron: Double = 1.008665
+  val massElectron: Double = 0.0000549
+  val massCarbon: Double = 12.0107
+  val massHydrogen: Double = 1.00794
+  val massOxygen: Double = 32.9988
+}
