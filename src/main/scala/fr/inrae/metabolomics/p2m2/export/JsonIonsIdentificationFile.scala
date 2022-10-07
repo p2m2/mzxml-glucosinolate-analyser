5f6eca5a732b2818845d8d2a55074b68e23b44a6
Olivier Filangi <you@example.com>
2022-10-07 11:18:45 +0200

    add test serialization

--- src/main/scala/fr/inrae/metabolomics/p2m2/export/JsonIonsIdentificationFile.scala
+++ src/main/scala/fr/inrae/metabolomics/p2m2/export/JsonIonsIdentificationFile.scala
@@ -1,5 +1,14 @@
 package fr.inrae.metabolomics.p2m2.`export`
 
-case object JsonIonsIdentificationFile {
+import fr.inrae.metabolomics.p2m2.config.ConfigReader
+import fr.inrae.metabolomics.p2m2.output.IonsIdentification
 
+import java.io.File
+
+case object JsonIonsIdentificationFile {
+  def build(list: Seq[IonsIdentification],
+            familyMetabolite: String,
+            configJson: ConfigReader, out: File): Unit = {
+
+  }
 }
